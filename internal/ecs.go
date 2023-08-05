90fcebcab5a0b96278f58ba92deb66a6855fb4e5
thaim <thaim24@gmail.com>
2023-08-06 08:37:39 +0900

    move query function for task-definition to separate function

--- internal/ecs.go
+++ internal/ecs.go
@@ -12,6 +12,7 @@ import (
 
 type AwsEcsAPI struct {
 	awsCfg aws.Config
+	awsApi *ecs.Client
 }
 
 func NewAwsEcsAPI(c aws.Config) *AwsEcsAPI {
@@ -25,32 +26,46 @@ func (api *AwsEcsAPI) Query(resource string) (*ResultList, error) {
 		Service: "ecs",
 		Resource: resource,
 	}
+	var err error = nil
 
-	awsAPI := ecs.NewFromConfig(api.awsCfg)
+	api.awsApi = ecs.NewFromConfig(api.awsCfg)
 
 	switch resource {
 	case "task-definition":
-		listOutput, err := awsAPI.ListTaskDefinitions(context.Background(), nil)
-		if err != nil {
-			return nil, err
-		}
-		for _, arn := range listOutput.TaskDefinitionArns {
-			input := &ecs.DescribeTaskDefinitionInput{
-				TaskDefinition: aws.String(arn),
-				Include: []types.TaskDefinitionField{
-					types.TaskDefinitionFieldTags,
-				},
-			}
-			output, err := awsAPI.DescribeTaskDefinition(context.Background(), input)
-			if err != nil {
-				return nil, err
-			}
-
-			resultList.Results = append(resultList.Results, output)
-		}
+		resultList, err = api.queryTaskDefinition()
 	default:
 		log.Error().Msgf("resource '%s' not supported in ecs service", resource)
 		return nil, fmt.Errorf("resource '%s' not supported in ecs service", resource)
+	}
+
+	return resultList, err
+}
+
+func (api *AwsEcsAPI) queryTaskDefinition() (*ResultList, error) {
+	resultList := &ResultList{
+		Service: "ecs",
+		Resource: "task-definition",
+	}
+
+	awsAPI := ecs.NewFromConfig(api.awsCfg)
+
+	listOutput, err := api.awsApi.ListTaskDefinitions(context.Background(), nil)
+	if err != nil {
+		return nil, err
+	}
+	for _, arn := range listOutput.TaskDefinitionArns {
+		input := &ecs.DescribeTaskDefinitionInput{
+			TaskDefinition: aws.String(arn),
+			Include: []types.TaskDefinitionField{
+				types.TaskDefinitionFieldTags,
+			},
+		}
+		output, err := awsAPI.DescribeTaskDefinition(context.Background(), input)
+		if err != nil {
+			return nil, err
+		}
+
+		resultList.Results = append(resultList.Results, output)
 	}
 
 	return resultList, nil
