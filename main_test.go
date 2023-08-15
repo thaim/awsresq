e5a58bc36c41deb430ff68b90cfce613eab6459b
thaim <thaim24@gmail.com>
2023-08-15 20:33:40 +0900

    add test for getVersion

---
+++ main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetVersion(t *testing.T) {
+	cases := []struct {
+		name string
+		version string
+		expected string
+	}{
+		{
+			name: "value",
+			version: "0.1.0",
+			expected: "0.1.0",
+		},
+		{
+			name: "value",
+			version: "",
+			expected: "(devel)",
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			defaultVersion := version
+			version = tt.version
+			actual := getVersion()
+
+			if actual != tt.expected {
+				t.Errorf("getVersion() = %v, want %v", actual, tt.expected)
+			}
+			version = defaultVersion
+		})
+	}
+}
