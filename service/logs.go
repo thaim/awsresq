d5b7d2ceac6bf86fbce9f21fa0d02ea0bfd83a54
thaim <thaim24@gmail.com>
2023-08-26 21:04:48 +0900

    use goroutine to call each region api

--- service/logs.go
+++ service/logs.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
@@ -38,16 +39,22 @@ func (api AwsresqLogsAPI) Query(resource string) (*ResultList, error) {
 
 	switch resource {
 	case "log-group":
+		ch := make(chan ResultList)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
 		for _, r := range api.region {
-			awsAPI := cloudwatchlogs.NewFromConfig(api.awsCfg, func(o *cloudwatchlogs.Options) {
-				o.Region = r
-			})
-			listOutput, err := awsAPI.DescribeLogGroups(context.Background(), nil)
-			if err != nil {
-				return nil, err
-			}
-			for _, lg := range listOutput.LogGroups {
-				resultList.Results = append(resultList.Results, lg)
+			go api.queryLogGroup(ctx, ch, r)
+		}
+
+		for _ = range api.region {
+			select {
+			case result := <-ch:
+				if result.Results != nil {
+					resultList.Results = append(resultList.Results, result.Results...)
+				}
+			case <-ctx.Done():
+				return nil, ctx.Err()
 			}
 		}
 	default:
@@ -56,4 +63,26 @@ func (api AwsresqLogsAPI) Query(resource string) (*ResultList, error) {
 	}
 
 	return resultList, nil
+}
+
+func (api *AwsresqLogsAPI) queryLogGroup(ctx context.Context, ch chan ResultList, r string) {
+	resultList := ResultList{
+		Service: "logs",
+		Resource: "log-group",
+	}
+
+	awsAPI := cloudwatchlogs.NewFromConfig(api.awsCfg, func(o *cloudwatchlogs.Options) {
+		o.Region = r
+	})
+	listOutput, err := awsAPI.DescribeLogGroups(ctx, nil)
+	if err != nil {
+		log.Error().Msgf("error querying log groups in region %s: %s", r, err)
+		return
+	}
+
+	for _, lg := range listOutput.LogGroups {
+		resultList.Results = append(resultList.Results, lg)
+	}
+
+	ch <- resultList
 }
