cf053c66675b3b06f7d0235e5e8b7753f4c863ae
thaim <thaim24@gmail.com>
2023-10-14 23:34:36 +0900

    add test for iam user

--- service/iam_test.go
+++ service/iam_test.go
@@ -121,3 +121,79 @@ func TestIamRoleQuery(t *testing.T) {
 		})
 	}
 }
+
+func TestIamUserQuery(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mc := mock_service.NewMockawsIamAPI(ctrl)
+
+	mc.EXPECT().
+		ListUsers(gomock.Any(), nil).
+		Return(&iam.ListUsersOutput{
+			Users: []types.User{
+				{
+					UserName: aws.String("test-user"),
+				},
+			},
+		}, nil).
+		AnyTimes()
+
+	cases := []struct {
+		name      string
+		expected  []types.User
+		wantErr   bool
+		expectErr string
+	}{
+		{
+			name: "valid user query",
+			expected: []types.User{
+				{
+					UserName: aws.String("test-user"),
+				},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			config, _ := config.LoadDefaultConfig(context.TODO())
+			api := NewAwsresqIamAPI(config, []string{"us-east-1"})
+			api.apiClient["us-east-1"] = mc
+
+			actual, err := api.Query("user")
+
+			if tt.wantErr {
+				if err == nil {
+					t.Errorf("expected error, but got nil")
+				}
+				if !strings.Contains(err.Error(), tt.expectErr) {
+					t.Errorf("expected %v, but got %v", tt.expectErr, err.Error())
+				}
+			}
+			if err != nil {
+				t.Errorf("expected nil, but got %v", err.Error())
+			}
+
+			if actual.Service != "iam" {
+				t.Errorf("expected iam, but got %v", actual.Service)
+			}
+			if actual.Resource != "user" {
+				t.Errorf("expected user, but got %v", actual.Resource)
+			}
+
+			if len(tt.expected) != len(actual.Results) {
+				t.Errorf("expected %v, but got %v", len(tt.expected), len(actual.Results))
+			}
+
+			for i := range tt.expected {
+				actualOutput, ok := actual.Results[i].(types.User)
+				if !ok {
+					t.Errorf("expected types.User, but got %T", actual.Results[i])
+				}
+				if !reflect.DeepEqual(actualOutput.UserName, tt.expected[i].UserName) {
+					t.Errorf("expected %v, but got %v", tt.expected[i].UserName, actualOutput.UserName)
+				}
+			}
+		})
+	}
+}
